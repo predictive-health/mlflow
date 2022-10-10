7dcead7fe5a31e5749e8d9b900c6cdce88ac9f0c
Predictive Health <73752262+predictive-health@users.noreply.github.com>
2022-10-10 11:08:05 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,8 @@ sudo apt install python3.9 -y
 sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev -y
 python3.9 --version
 sudo apt install python3-pip -y 
-pip install mlflow[extras]
+pip install mlflow
+pip install scikit-learn
 cd ..
 wget https://repo.anaconda.com/miniconda/Miniconda3-py39_4.12.0-Linux-x86_64.sh
 wget https://repo.anaconda.com/archive/Anaconda3-2022.05-Linux-x86_64.sh
